151ce5211963207603c4b7d4a842d5a4f33a72c2
Philipp <85802326+KnowOneWasTaken@users.noreply.github.com>
2024-07-24 03:40:04 +0200

    Audio-switches

--- scenes/options.gd
+++ scenes/options.gd
@@ -1,5 +1,10 @@
 extends Control
-
+var music = true
+var sfx = true
+@onready var music_switch = $"Control/CenterContainer/VBoxContainer/music-switch"
+@onready var sfx_switch = $"Control/CenterContainer/VBoxContainer/sfx-switch"
+const SWITCH_OFF = preload("res://assets/original/switch_off.png")
+const SWITCH_ON = preload("res://assets/original/switch_on.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,9 +12,30 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 
 func _on_settings_button_up():
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+
+func _on_musicswitch_button_up():
+	music = not music
+	match music:
+		false: 
+			music_switch.icon = SWITCH_OFF
+		true: 
+			music_switch.icon = SWITCH_ON
+		_: 
+			music_switch.icon = SWITCH_ON
+	var index = AudioServer.get_bus_index("Music")
+	AudioServer.set_bus_mute(index, music)
+
+func _on_sfxswitch_button_up():
+	sfx = not sfx
+	match sfx:
+		false: sfx_switch.icon = SWITCH_OFF
+		true: sfx_switch.icon = SWITCH_ON
+		_: sfx_switch.icon = SWITCH_ON
+	var index = AudioServer.get_bus_index("SFX")
+	AudioServer.set_bus_mute(index, sfx)
